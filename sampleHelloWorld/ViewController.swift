429e8b27dfa758a1e9469becded29afa553b71ab
Riho Okubo <rihoohkubo1002@gmail.com>
2016-04-28 17:35:53 +0800

    no message

--- sampleHelloWorld/ViewController.swift
+++ sampleHelloWorld/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    @IBOutlet weak var myLabel: UILabel!
+   
+    @IBOutlet weak var myButton: UIButton!
+  
+    
+    //画面の起動時に実行されるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+        
+        myLabel.text = "こんにちは！"
+        
+        print(myLabel.text)
+        
+        myLabel.text = "コンニチハ！"
+        
+        print(myLabel.text)
+        
+        
     }
-
+    
+    //myButtonが押された時
+    @IBAction func btnTab2(sender: AnyObject) {
+        myLabel.text = "こんにちは、セブ！"
+    }
+   
+    @IBAction func btnTab(sender: UIButton) {
+        
+        myLabel.text = "こんにちは、世界！"
+    }
+    
+    
+    
+    //デバイスがメモリ不足になった時の
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
